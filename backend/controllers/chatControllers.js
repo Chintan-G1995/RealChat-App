3d3ece145c3bda841afd9970939f034cec80c53b
Chintan-G1995 <153381573+Chintan-G1995@users.noreply.github.com>
2024-07-02 12:59:53 +0530

    backend chatRouter create group chat

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,48 +1,95 @@
 import asyncHandler from "express-async-handler";
 import Chat from "../models/Chat.js";
 
-//description   create or fetch one-on -one chat
-//Path          POST/api/v1/chat
-//access        Private
+//@description   Create or fetch one-on-one chat
+//@Path          POST /api/v1/chat
+//@access         Private 
+export const accessChat=asyncHandler(async(req,res)=>{
+    const {userId}=req.body
 
-export const accessChat = asyncHandler(async (req, res) => {
-  const { userId } = req.body;
+    if(!userId){
+        console.log("User id not sent");
+        res.sendStatus(400)
+    }
 
-  if (!userId) {
-    console.log("UserId param not sent with request");
-    return res.status(400);
-  }
+    let isChat=await Chat.find({
+        isGroupChat:false,
+        $and:[{users:{$elemMatch:{$eq:userId}}},{users:{$elemMatch:{$eq:req.userId}}}]
+    }).populate("users","-password","-confirmPassword").populate("latestMessage")
 
-  let isChat=await Chat.find({
-    isGroupChat:false,
-    $and:[{users:{$elemMatch:{$eq:userId}}},{users:{$elemMatch:{$eq:req.userId}}}]
-}).populate("users","-password","-confirmPassword").populate("latestMessage")
+    isChat=await Chat.populate(isChat,{
+        path:"latestMessage.sender",
+        select:"name email photo"
+    })
 
-isChat=await Chat.populate(isChat,{
-    path:"latestMessage.sender",
-    select:"name email photo"
+    if(isChat.length>0){
+        res.send(isChat[0])
+    }else{
+        var chatData={
+            chatName:"Sender",
+            isGroupChat:false,
+            users:[userId,req.userId]
+        }
+    }
+    try {
+        let newChat=await Chat.create(chatData)
+
+        newChat=await Chat.findById(newChat._id).populate("users","name email photo")
+
+        res.send(newChat)
+    } catch (error) {
+        let err=new Error(error.message)
+        next(err)
+    }
 })
 
-  if (isChat.length > 0) {
-    res.send(isChat[0]);
-  } else {
-    var chatData = {
-      chatName: "Sender",
-      isGroupChat: false,
-      users: [req.userId, userId],
-    };
+//@description   fetch all chats for the logged in user
+//@Path          GET /api/v1/chat
+//@access         Private 
+export const fetchChats=asyncHandler(async (req,res,next)=>{
+    let chats=await Chat.find({users:{$elemMatch:{$eq:req.userId}}}).populate("users","-password").populate("groupAdmin","-password").populate("latestMessage").sort({updatedAt:-1})
+
+    let finalChats=await Chat.populate(chats,{
+        path:"latestMessage.sender",
+        select:"name email photo"
+    })
+    res.status(200).json(finalChats)
+}) 
+
+//@description   create group chat
+//@Path          post /api/v1/chat
+//@access         Private 
+
+export const createGroup=asyncHandler(async (req,res,next)=>{
+    if(!req.body.users || !req.body.chatName){
+        res.status(400).json("Please fill all the fields")
+    }
+
+    let users=JSON.parse(req.body.users.replace(/'/g,'"'));
+
+    if(users<2){
+        res.status(400).json("To create a group there should be more than 2 users")
+    }
+
+    users.push(req.userId)
 
     try {
-      let newChat = await Chat.create(chatData);
+        let groupChat=await Chat.create({
+            chatName:req.body.chatName,
+            isGroupChat:true,
+            users:users,
+            groupAdmin:req.userId,
+        })
 
-      newChat = await Chat.findById(newChat._id)
-        .populate("users", "name email photo")
-        .populate("latestMessage");
+        groupChat=await Chat.findById(groupChat._id).populate("users","-password -confirmPassword").populate("groupAdmin","-password -confirmPassword").populate("latestMessage")
 
-      res.status(200).send(newChat);
+        groupChat=await Chat.populate(groupChat,{
+            path:"latestMessage.sender",
+            select:"name email photo"
+        })
+        res.status(201).json(groupChat)
     } catch (error) {
-      console.error(error);
-      res.status(400).json({ error: error.message });
+        let err=new Error(error.message)
+        next(err)
     }
-  }
-});
+})
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,8 +1,12 @@
-import {Router} from "express"
-import { accessChat } from "../controllers/chatControllers.js";
+import { Router } from "express";
 import auth from "../middleware/auth.js";
+import { accessChat, createGroup, fetchChats } from "../controllers/chatControllers.js";
+
 let chatRouter=Router()
 
-chatRouter.post("/",auth,accessChat)
+chatRouter.post("/",auth,accessChat);
+chatRouter.get("/",auth,fetchChats);
+chatRouter.post("/group",auth,createGroup);
+
 
 export default chatRouter;
